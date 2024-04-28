3b2cc6e3025485ca2fdaf1eb8412fae73e8da8a8
Vlad Gapanovich <vladgapanovich172@gmail.com>
2024-04-29 00:02:33 +0300

    feat: add nav link component

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx"
 import styles from "@/styles/header/index.module.scss"
 import Logo from "@/components/elements/Logo/Logo"
+import NavLink from "@/components/elements/NavLink/NavLink"
+import { RouteType } from "@/types/route"
 
 interface HeaderProps {}
 
@@ -9,6 +11,7 @@ const Header = () => {
     <header className={clsx(styles.header)}>
       <div className={clsx("container", styles.header__container)}>
         <Logo />
+        <NavLink text={"Category"} />
       </div>
     </header>
   )
--- types/route.ts
+++ types/route.ts
@@ -1,3 +1,4 @@
 export enum RouteType {
-  MAIN = '/'
+  MAIN = '/',
+  CAT = "/cat"
 }
