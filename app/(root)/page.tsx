b5511d4daf6a8055527ecf924bcc17d9ccf3d4c4
Vlad Gapanovich <vladgapanovich172@gmail.com>
2024-05-05 22:34:53 +0300

    feat: add folder (root), rename (sign) to (auth)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import React from "react"
 import MainPage from "@/components/templates/MainPage/MainPage"
+
 
 export default function HomePage() {
   return <MainPage />
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-          <Layout>{children}</Layout>
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   )
